0c8bf6fd421b45ae15b7b2ad516912163b091dbf
retro49 <retroin49@gmail.com>
2023-07-05 21:06:33 -0400

    added comment in logging fixed typo in gitignore

--- logging/log.py
+++ logging/log.py
@@ -1,11 +1,15 @@
 import sys
 
 
+""" logging class """
+
+
 class Log:
     __GREEN = '\033[1;32m'
     __NORM = '\033[0;00m'
     __RED = '033[1;31m'
 
+    """ debugging purpose """
     @staticmethod
     def d(key: str, message: str):
         sys.stdout.write(
@@ -16,6 +20,7 @@ class Log:
             chr(0x0a)
         )
 
+    """ error mode """
     @staticmethod
     def e(key: str, message: str):
         sys.stdout.write(
@@ -25,6 +30,7 @@ class Log:
             chr(0x0a)
         )
 
+    """ normal writer """
     @staticmethod
     def w(message: str):
         sys.stdout.write(Log.__NORM + str(message) + chr(0x0a))
